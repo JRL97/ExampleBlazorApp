a485cee4c283ed1b36a0d1e714072f0519915fcb
JRL97 <91621546+JRL97@users.noreply.github.com>
2022-12-03 17:21:20 +0000

    Finished Video 4

---
+++ ExampleBlazorApp/Models/ShoppingItem.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ExampleBlazorApp.Models
+{
+    public class ShoppingItem
+    {
+        public int Id { get; set; } 
+
+        public Product Product { get; set; }
+
+        [Range(1,9)]
+        public int Quanitity { get; set; } = 1;
+
+        [Range(1, 50000.00)]
+        public decimal PurchasePrice { get; set; } = 0;
+
+    }
+}
